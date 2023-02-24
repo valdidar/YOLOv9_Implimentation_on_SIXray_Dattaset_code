d430d8a239571de38fa0f8c5e5fc511b41b1b06c
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-02-24 14:38:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ fetch('https://icanhazdadjoke.com/slack')
     .then(data => data.json())
     .then(jokeData => {
         const jokeText = jokeData.attachments[0].text;
-        const jokeElement = document.getElementById('jokeElement');
-
-        jokeElement.innerHTML = jokeText;
+            const jokeElement = document.getElementById('jokeElement');
+    
+            jokeElement.innerHTML = jokeText;
+			const synth = window.speechSynthesis;
+			const utterance = new SpeechSynthesisUtterance(jokeText);
+			utterance.lang = 'en-US';
+			utterance.rate = 1.0;
+			synth.speak(utterance);
     })
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("buton").addEventListener("click", function() {
@@ -15,6 +20,11 @@ document.addEventListener("DOMContentLoaded", function() {
             const jokeElement = document.getElementById('jokeElement');
     
             jokeElement.innerHTML = jokeText;
+			const synth = window.speechSynthesis;
+			const utterance = new SpeechSynthesisUtterance(jokeText);
+			utterance.lang = 'en-US';
+			utterance.rate = 1.0;
+			synth.speak(utterance);
         })
     });
   });
