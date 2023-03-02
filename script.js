d291c8e0c841cd8f02d420cd4dff0d370774128b
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-03-02 15:51:38 +0530

    0.0.3

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-fetch('https://icanhazdadjoke.com/slack')
-    .then(data => data.json())
-    .then(jokeData => {
+fetch("https://icanhazdadjoke.com/slack")
+    .then((data) => data.json())
+    .then((jokeData) => {
         const jokeText = jokeData.attachments[0].text;
-            const jokeElement = document.getElementById('jokeElement');
-    
-            jokeElement.innerHTML = jokeText;
-			const synth = window.speechSynthesis;
-			const utterance = new SpeechSynthesisUtterance(jokeText);
-			utterance.lang = 'en-US';
-			utterance.rate = 1.0;
-			synth.speak(utterance);
-    })
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("buton").addEventListener("click", function() {
-        fetch('https://icanhazdadjoke.com/slack')
-        .then(data => data.json())
-        .then(jokeData => {
-            const jokeText = jokeData.attachments[0].text;
-            const jokeElement = document.getElementById('jokeElement');
-    
-            jokeElement.innerHTML = jokeText;
-			const synth = window.speechSynthesis;
-			const utterance = new SpeechSynthesisUtterance(jokeText);
-			utterance.lang = 'en-US';
-			utterance.rate = 1.0;
-			synth.speak(utterance);
-        })
+        const jokeElement = document.getElementById("jokeElement");
+
+        jokeElement.innerHTML = jokeText;
+
+        const synth = window.speechSynthesis;
+        const chunks = jokeText.split(/[\.,\?]\s/);
+        let i = 0;
+        const speakChunks = () => {
+            if (i < chunks.length) {
+                const utterance = new SpeechSynthesisUtterance(chunks[i] + (/[\.,\?]$/.test(chunks[i]) ? '' : '.'));
+                utterance.lang = "en-US";
+                utterance.rate = parseInt(1.0);
+                synth.speak(utterance);
+                i++;
+                utterance.onend = speakChunks;
+            }
+        };
+        speakChunks();
     });
-  });
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("buton").addEventListener("click", function () {
+        fetch("https://icanhazdadjoke.com/slack")
+            .then((data) => data.json())
+            .then((jokeData) => {
+                const jokeText = jokeData.attachments[0].text;
+                const jokeElement = document.getElementById("jokeElement");
+
+                jokeElement.innerHTML = jokeText;
+
+                const synth = window.speechSynthesis;
+                const chunks = jokeText.split(/[\.,\?]\s/)
+                let i = 0;
+                const speakChunks = () => {
+                    if (i < chunks.length) {
+                        const utterance = new SpeechSynthesisUtterance(chunks[i] + (/[\.,\?]$/.test(chunks[i]) ? '' : '.'));
+                        utterance.lang = "en-US";
+                        utterance.rate = parseInt(1.0);
+                        synth.speak(utterance);
+                        i++;
+                        utterance.onend = speakChunks;
+                    }
+                };
+                speakChunks();
+            });
+    });
+});
+
